327684121ee19b68b76566478099d84c481bc658
YuliiaPanchuk <jula1panchuk@gmail.com>
2021-12-13 09:00:50 +0100

    Task7

--- Assignment3/Task7/Task7.cpp
+++ Assignment3/Task7/Task7.cpp
@@ -7,34 +7,40 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-// Declare prototypes
+// Declare prototypes and variables
 bool leapYear(int year);
+int year;
 
 int main()
 {
 	cin >> year;
+	char answer = 'Y';
 
-	if (leapYear(year))
-		cout << "This is a leap year!" << endl;
-	else
-		cout << "This is not a leap year!" << endl;
+	do
+	{
+		if (leapYear(year))
+		{
+			cout << "This is a leap year!" << endl;
+		}
+		else
+		{
+			cout << "This is not a leap year!" << endl;
+		}
+	} while (toupper(answer) == 'Y');
+
 }
 
 bool leapYear(int year)
 {
-	int year;
-
-	cout << "Enter year: " << endl;
-	cin >> year;
 	// If a year can be devided by 4 and can not be divisible by 100, 
 	// then it is a leap year. As well as if a year can be devided by 400 it will be leap year.
 	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
-		cout << year << " is a leap year";
+		year = true;
 	}
 	else
 	{
-		cout << year << " is not a leap year";
+		year = false;
 	}
-	return 0;
+	return year;
 }
